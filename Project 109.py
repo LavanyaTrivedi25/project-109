dad6d4ce397d78785dac39ffe64a766bcc97f6c1
Lavanya Trivedi <85881382+LavanyaTrivedi25@users.noreply.github.com>
2022-08-30 16:58:26 +0530

    Update and rename project 109.py to Project 109.py

--- Project 109.py
+++ Project 109.py
@@ -45,12 +45,23 @@ while True:
 
              #checking if all fingers are folded
             if all(finger_fold_status):
+                # take a screenshot of the screen and store it in memory, then
+                # convert the PIL/Pillow image to an OpenCV compatible NumPy array
+                # and finally write the image to disk
                 image = pyautogui.screenshot()
-                image = cv2.cvtColor(np.array(image), cv2.Color_RGB2BGR)
+                image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
                 cv2.imwrite("in_memory_to_disk.png", image)
+
+                # this time take a screenshot directly to disk
                 pyautogui.screenshot("straight_to_disk.png")
+
+                # we can then load our screenshot from disk in OpenCV format
                 image = cv2.imread("straight_to_disk.png")
-                cv2.imshow("Screenshot", imutils.resize(image, width = 600))
+                cv2.imshow("Screenshot", imutils.resize(image, width=600))
+
+
+
+
 
             mp_draw.draw_landmarks(img, hand_landmark,
             mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0,0,255),2,2),
